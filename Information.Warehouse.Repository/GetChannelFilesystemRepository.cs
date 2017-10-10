9e7829abc8e2fadfe15581cab3d1891be80e4f55
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2017-10-10 23:12:26 +0200

    - added repository to retrieve channel information from filesystem - fixed folder naming

---
+++ Information.Warehouse.Repository/GetChannelFilesystemRepository.cs
@@ -0,0 +1,12 @@
+﻿using Information.Warehouse.Entity;
+
+namespace Information.Warehouse.Repository
+{
+  public class GetChannelFilesystemRepository : IGetChannelRepository
+  {
+    public Channel GetChannel()
+    {
+      return new Channel();
+    }
+  }
+}
