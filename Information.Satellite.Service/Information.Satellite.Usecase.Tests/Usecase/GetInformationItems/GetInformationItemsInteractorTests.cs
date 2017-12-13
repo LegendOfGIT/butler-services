907a1872abfba6031f6a28c14298f0aa9995b7bd
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2017-12-13 23:44:45 +0100

    - first version of information.satellite.service

---
+++ Information.Satellite.Service/Information.Satellite.Usecase.Tests/Usecase/GetInformationItems/GetInformationItemsInteractorTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Information.Satellite.Usecase.GetInformationItems
+{
+  public class GetInformationItemsInteractorTests
+  {
+    [Fact]
+    public void TestFirstTest()
+    {
+      Assert.True(true);
+    }
+  }
+}
